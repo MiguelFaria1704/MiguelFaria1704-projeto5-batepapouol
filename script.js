411b2945d4069640ede8d249130023e99f1ff35f
Miguel Faria <mattar1704miguel@gmail.com>
2022-06-25 16:35:31 -0300

    manter cnsexão

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let userName = {
+    name: ""
+}
+
 function signIn () {
-    const userName = {
-        name: prompt("Qual o seu lindo nome?")
-    };
+    userName.name = prompt("Qual o seu lindo nome?");
     
     let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", userName);
 
@@ -9,11 +11,13 @@ function signIn () {
     promise.catch(handleError);
 }
 
-signIn ();
+signIn();
 
 function handleError (error) {
     alert("Este nome ja existe! Escolha outro.");
     signIn ();
 }
 
+let keepLoggedIn = () => axios.post("https://mock-api.driven.com.br/api/v6/uol/status", userName);
 
+setInterval(keepLoggedIn, 5000);
